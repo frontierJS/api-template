8687a107a356ae5520ed06587ac01872048bd9d0
Jordan <knightworks@pm.me>
2020-01-18 23:40:40 -0600

    beta

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ require('@frontierjs/toolbelt/lib/require')
 
 const { env } = require('@frontierjs/backend');
 
-let port = env.get('PORT') || 3000;
+let port = env.get('PORT') //Frontier default is 9999
+
 let server = require('./Server/server')
-server.listen(port, () => console.log(`Server listening on port ${port}!`))
+
+server.listen(port, () => console.log(`
+  Server listening on port ${port}!
+`))
